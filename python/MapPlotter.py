9283ddcb6ac6f8b4eea6e6db56e49bca913735be
Chung-Dial.Lim <chungdial@gmail.com>
2017-06-11 19:15:35 +0800

    [Python] Refine function name

--- python/MapPlotter.py
+++ python/MapPlotter.py
@@ -61,7 +61,7 @@ class MapPlotter:
 		return result
 
 	#map.html?current_location=25.257738,121.473656&event_location=25.357738,121.573656;25.757738,121.673656;25.257738,121.173656&event_name=停水,停電,修路&event_time=2017-06-15:09:30:00~2017-06-16:16:00:00
-	def drawMarkerBySingleId(self,userIdLists,current_location):
+	def drawMarkerById(self,userIdLists,current_location):
 		url_result = self.hosting_url + "map.html?"
 		url_location = ""
 		event_location = ""
--- python/example.py
+++ python/example.py
@@ -4,4 +4,4 @@ from MapPlotter import MapPlotter
 userId_list = {'c8eeb9a44dfe11e7ab03','c8f4b2324dfe11e7ab03','cd4e5d924dfe11e7ab03','c8fcc5e44dfe11e7ab03'};
 current_location = {'latitude':'25.0339','longitude':'121.5645'}; #latitude, longitude
 x = MapPlotter('https://livelihood-api.herokuapp.com/events?ids=','http://www.3drens.tw/');
-x.drawMarkerBySingleId(userId_list,current_location);
+x.drawMarkerById(userId_list,current_location);
