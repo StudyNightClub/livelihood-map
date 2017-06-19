d5510042e2d8c6c951f3f03cd0359c9d5ba38cbf
Ching Lin <ikafire1230@gmail.com>
2017-06-20 00:54:49 +0800

    [Python] Bump version number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='mapplotter',
-      version='1.1.3',
+      version='2.0.0',
       description='Generate map URL given livelihood event ids.',
       url='https://github.com/StudyNightClub/livelihood_map',
       author='Chungdial Lin',
